3076d5b97b5d8a93f5e15756f53fc9e83a594744
hdj <hdj@addcn.com>
2021-01-14 14:18:15 +0800

    support docker development

---
+++ src/Aspect/RegisterServiceAspect.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of service-governance-extra.
+ *
+ * @link     https://github.com/friendsofhyperf/service-governance-extra
+ * @document https://github.com/friendsofhyperf/service-governance-extra/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+namespace FriendsOfHyperf\ServiceGovernanceExtra\Aspect;
+
+use Hyperf\Di\Annotation\Aspect;
+use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+
+/**
+ * @Aspect
+ */
+class RegisterServiceAspect extends AbstractAspect
+{
+    public $classes = [
+        'Hyperf\ServiceGovernance\Listener\RegisterServiceListener::getServers',
+        'FriendsOfHyperf\ServiceGovernanceExtra\Handler\DeregisterServicesHandler::getServers',
+    ];
+
+    public function process(ProceedingJoinPoint $proceedingJoinPoint)
+    {
+        $servers = $proceedingJoinPoint->process();
+
+        if (env('HOST_MACHINE_ADDR')) {
+            foreach ($servers as [&$host, $port]) {
+                $host = env('HOST_MACHINE_ADDR');
+            }
+        }
+
+        return $servers;
+    }
+}
